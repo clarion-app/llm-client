5df45d0f7feeac99b308e6f739ae67a75848edac
Tim Schwartz <tim@metaverse.systems>
2024-05-17 08:57:38 -0500

    Add user agent to http client

--- src/Controllers/FetchPageController.php
+++ src/Controllers/FetchPageController.php
@@ -15,8 +15,12 @@ class FetchPageController extends Controller
         $url = $request->input('url');
 
         $host = 'http://localhost:9515'; // ChromeDriver default URL
+        $agent = "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/111.0";
         $chromeOptions = new ChromeOptions();
         $chromeOptions->addArguments(['--headless']);
+        $chromeOptions->addArguments(array(
+            '--user-agent=' . $agent
+        ));
         $capabilities = \Facebook\WebDriver\Remote\DesiredCapabilities::chrome();
         $capabilities->setCapability(ChromeOptions::CAPABILITY_W3C, $chromeOptions);
         $driver = ChromeDriver::start($capabilities);
